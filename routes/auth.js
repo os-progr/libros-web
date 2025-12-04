7808858ae2fa5d5768c722f661d041c089d75853
edaninguna <edaninguna@gmail.com>
2025-12-04 18:51:20 -0500

    fix: Corregir importación de passport en routes/auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@
 
 const express = require('express');
 const router = express.Router();
-const passport = require('../config/passport');
+const passport = require('../config/google-auth');
 
 // @route   GET /auth/google
 // @desc    Initiate Google OAuth flow
